5d7c74dc425ee474dcb6da0a64d8ad18879e453f
Oleksandr <oleksandr.ostrovchuk@gmail.com>
2023-05-10 08:25:05 +0200

    comment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+// hide the block
+
 let hideParahraf = document.querySelector('.update');
 if (hideParahraf) {
   hideParahraf.onclick = function () {
-      document.querySelector('.update').style.display = 'none';
+    document.querySelector('.update').style.display = 'none';
   };
 };
